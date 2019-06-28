9ce3b54af177fdf08bce64dda6ea9a2645eac81a
tim23x <tim23x@hotmail.com>
2019-06-28 12:14:02 +0100

    Update serial.js

--- demos/serial.js
+++ demos/serial.js
@@ -12,11 +12,12 @@ var serial = {};
   serial.requestPort = function() {
     const filters = [
       { 'vendorId': 0x2341, 'productId': 0x8036 },
-      { 'vendorId': 0x2341, 'productId': 0x8037 },
+      { 'vendorId': 0x2341, 'productId': 0x8037 },//Adruino Micro
       { 'vendorId': 0x2341, 'productId': 0x804d },
       { 'vendorId': 0x2341, 'productId': 0x804e },
       { 'vendorId': 0x2341, 'productId': 0x804f },
       { 'vendorId': 0x2341, 'productId': 0x8050 },
+      { 'vendorId': 0x239A, 'productId': 0x802B },//Adafruit Itsy Bitsy
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
